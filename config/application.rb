cea679739d7ac77884c1f85588b7ca648dd71474
ghclo <min424@live.cn>
2014-05-20 16:38:23 +0800

    tell the Rails generator to skip unit tests for helpers ad views

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,17 @@ module Myapp
     # config.i18n.default_locale = :de
 
     config.assets.initialize_on_precompile=false
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+    
   end
 end
