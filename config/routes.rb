5e557f07b8458deb0b7b5c4d48a2d88a043fa0ae
ghclo <min424@live.cn>
2014-05-23 16:47:32 +0800

    user profiles &restrict access

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
-
   authenticated :user do
-    root :to => 'home#index'
+    resources :to => 'home#index'
   end
   root :to => "home#index"
   devise_for :users
+  resources :users
+  # get 'users/index'
+
+  # get 'users/show'
+
+  # get 'home/index'
+
+  
   # root 'welcome#index'
+  get 'welcome/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
