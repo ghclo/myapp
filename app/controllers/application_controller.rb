acc67abba88f2fc9637ac6ac037927c10fac304c
ghclo <min424@live.cn>
2014-05-28 17:17:18 +0800

    fixed devise strong parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 
   # Redirect to a specific page on successful sign in out 
   def after_sign_in_path_for(resource)
